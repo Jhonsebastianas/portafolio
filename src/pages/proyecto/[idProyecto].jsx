228c92453975a08a4a6e37c02753ca26e5428936
Jhon Sebastian <Sebastamriel@gmail.com>
2021-02-27 21:07:31 -0500

    Se actualizan proyectos - 27/02/2021, 09:07 p.m.

--- src/pages/proyecto/[idProyecto].jsx
+++ src/pages/proyecto/[idProyecto].jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
 import ImageNext from 'next/image'
 import Proyectos from '@constants/Proyects'
-import { Grid, Header, List, Segment } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, List, Segment } from 'semantic-ui-react';
 
 export const getStaticProps = async ({ params }) => {
     const { idProyecto } = params;
@@ -32,13 +33,54 @@ export const getStaticPaths = async () => {
 }
 
 const Proyecto = ({ proyecto }) => {
-    const { id, nombre, descripcion, habilidades, cargo, urlSitio, imagen } = proyecto
+    const { id, nombre, descripcion, habilidades, cargo, urlSitio, imagen, empresa, tiempo } = proyecto
     const srcImage = `/images/projects/${imagen}`
     return (
-        <>
+        <div className="contenedor-proyecto">
             <Segment vertical>
                 <Grid stackable columns='equal'>
-                    <Grid.Column>
+                    <Grid.Column width={9}>
+                        <Header as='h1' icon textAlign='left'>
+                            <Header.Content><a href={urlSitio} target="_blank">{nombre}</a></Header.Content>
+                        </Header>
+                        <Grid.Column width={16}>
+                            <p>{descripcion}</p>
+                        </Grid.Column>
+                        <br></br>
+                        <br></br>
+                        <Grid width={16}>
+                            <Grid.Column width={6}>
+                                <h3>Cargo</h3>
+                                {cargo}
+                            </Grid.Column>
+                            <Grid.Column width={5}>
+                                <h3>Año</h3>
+                                {tiempo}
+                            </Grid.Column>
+                            <Grid.Column width={5}>
+                                <h3>Empresa</h3>
+                                {empresa}
+                            </Grid.Column>
+                        </Grid>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <Grid.Column width={16}>
+                            <Button><Link href="/"><a>Inicio</a></Link></Button>
+                            <Button.Group>
+                                {id != 1 && <>
+                                    <Button><Link href={`/proyecto/${parseInt(id) - 1}`}><a>Anterior</a></Link></Button>
+                                </>}
+                                {(id != 1 && id != Proyectos.length) && <Button.Or text="ver" />}
+                                {id != Proyectos.length && <>
+                                    <Button><Link href={`/proyecto/${parseInt(id) + 1}`}><a>Siguiente</a></Link></Button>
+                                </>}
+                            </Button.Group>
+
+                        </Grid.Column>
+                    </Grid.Column>
+
+                    <Grid.Column width={7}>
                         <ImageNext
                             src={srcImage}
                             width="1200px"
@@ -46,31 +88,9 @@ const Proyecto = ({ proyecto }) => {
                             alt={imagen}
                         />
                     </Grid.Column>
-                    <Grid.Column>
-                        <Header as='h1' icon textAlign='left'>
-                            <Header.Content>{nombre}</Header.Content>
-                        </Header>
-                        <Grid.Column width={16}>
-                            <h2>{cargo}</h2>
-                        </Grid.Column>
-                        <Grid.Column width={16}>
-                            <p>{descripcion}</p>
-                        </Grid.Column>
-                        <br></br>
-                        <Grid.Column width={16}>
-                            <span>{habilidades}</span>
-                        </Grid.Column>
-                        <br></br>
-                        <Grid.Column width={16}>
-                            Ver sitio: <a href={urlSitio} target="_blank">{nombre}</a>
-                            <br></br><br></br>
-                            <a href="/">volver al inicio</a>
-                        </Grid.Column>
-                    </Grid.Column>
                 </Grid>
             </Segment>
-
-        </>
+        </div>
     )
 }
 
