4837d5e2d170c5b1975fa993445f8c48f8903c01
Jhon Sebastian <Sebastamriel@gmail.com>
2021-03-15 13:00:44 -0500

    Se sube nuevo proyecto de tagmascotas - 15/03/2021, 01:00 p.m.

--- src/constants/Proyects.js
+++ src/constants/Proyects.js
@@ -11,6 +11,19 @@ const projects = [
             "html": "Controlsep es un proyecto creado, para incentivar y permitir a las personas, medir el consumo del agua y energía en sus hogares, de forma automática por medio de sensores, o manual, digitando los registros del contador público"
         }
     },
+    {
+        "node": {
+            "frontmatter": {
+                "title": "Tagmascota",
+                "tech": ["React.js", "Next.js", "SQL"],
+                "external": "https://tagmascotas.vercel.app/",
+                "github": "",
+                "cover": "Tagmascota.png",
+            },
+            "html": `Tagmascota es un proyecto que brindar un punto de reporte de mascotas perdidas y encontradas.
+            Aprovechamos varias tecnologías para la identificación previa y posteriormente la recuperación de las mascotas`
+        }
+    },
 ]
 
 export default projects
