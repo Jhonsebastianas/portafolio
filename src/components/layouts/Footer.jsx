e63e1d24384aa4254bd7c8e70eed3c3e913ae9cb
Jhon Sebastian <Sebastamriel@gmail.com>
2021-03-17 20:52:02 -0500

    Se quita contactos del footer - 17/03/2021, 08:51 p.m.

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
                     Realizado por Jhon Sebastian Agudelo Sierra
             </a>
             </footer>
-            <div>
+            {/* <div>
                 <Button circular color='facebook' rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/johnsebastian.agudelosierra.3/" icon='facebook' />
                 <Button circular color='twitter' rel="noopener noreferrer" target="_blank" href="https://twitter.com/JhonSebastianAS" icon='twitter' />
                 <Button circular color='linkedin' rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/jhonsabastianas/" icon='linkedin' />
                 <Button circular color='github' rel="noopener noreferrer" target="_blank" href="https://github.com/Segaretsu/" icon='github' />
                 <Button circular color='github' rel="noopener noreferrer" target="_blank" href="https://jhonsebastianas.medium.com/" icon='medium' />
-            </div>
+            </div> */}
             <br></br>
         </>
     )
