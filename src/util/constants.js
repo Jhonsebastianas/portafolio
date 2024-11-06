b346c78bab7fcae433061debd30a27051615e0b9
Sebastian Agudelo <sebastamriel@gmail.com>
2024-11-06 12:23:38 -0500

    Se suben constantes

--- src/util/constants.js
+++ src/util/constants.js
@@ -9,4 +9,6 @@ export const SOCIAL_MEDIA = Object.freeze({
     GITHUB: new SocialMedia('uil uil-github-alt', 'https://github.com/jhonsebastianas'),
     YOUTUBE: new SocialMedia('uil uil-youtube', 'https://www.youtube.com/@jhonsebastianas'),
     LINKEDIN: new SocialMedia('uil uil-linkedin-alt', 'https://www.linkedin.com/in/jhonsabastianas/'),
+    PERSONAL_BLOG : new SocialMedia('uil uil-document-layout-left', 'https://www.jhonsebastianas.com/blog'),
+    INSTAGRAM : new SocialMedia('uil uil-instagram', 'https://www.instagram.com/jhonsebastianas/'),
 });
