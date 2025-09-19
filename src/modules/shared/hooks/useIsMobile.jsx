6172d271ef49c1b52087431d6282b3b186b8331d
Sebastian Agudelo <sebastamriel@gmail.com>
2025-09-18 23:54:54 -0500

    change implementation

--- src/modules/shared/hooks/useIsMobile.jsx
+++ src/modules/shared/hooks/useIsMobile.jsx
@@ -1,21 +1,8 @@
+import { useMediaQuery } from "react-responsive";
 import { theme } from "@styles/theme";
-import { useState, useEffect } from "react";
-import useIsClient from "./useIsClient";
 
 function useIsMobile(breakpoint = theme.breakpoints.mobile) {
-  const isClient = useIsClient();
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    if (!isClient) return;
-
-    const checkMobile = () => setIsMobile(window.innerWidth < breakpoint);
-    checkMobile();
-
-    window.addEventListener("resize", checkMobile);
-    return () => window.removeEventListener("resize", checkMobile);
-  }, [isClient, breakpoint]);
-
+  const isMobile = useMediaQuery({ maxWidth: breakpoint });
   return isMobile;
 }
 
